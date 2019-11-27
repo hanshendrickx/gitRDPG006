8156da87a23b795a34c7c814add864f78886a4bb
Hans Hendrickx <hhlhendrickx@gmail.com>
2019-11-27 15:21:06 +0100

    Permissions
    A few errors were corrected in the code. I assume the corrections are OK, because it all seems to work.

--- mposp_root/userapi/views.py
+++ mposp_root/userapi/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from userapi.models import User
 from userapi.serializers import UserSerializer
+from rest_framework.permissions import AllowAny
+from mposp.permissions import IsLoggedInUserOrAdmin, IsAdminUser
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'list' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
